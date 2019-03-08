a355413132d8cdb825307f7c41e5432432c66dd2
huan <nguyenchanhhuan@gmail.com>
2019-03-08 22:27:00 +0700

    chapter_8

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
     {minimum: Settings.password_length_min}
   before_save{email.downcase!}
   has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
