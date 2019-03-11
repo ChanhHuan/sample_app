aa07f59977cf0135d79c5c7954766816dcc20cda
huan <nguyenchanhhuan@gmail.com>
2019-03-11 13:41:31 +0700

    chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   validates :password, presence: true, length:
-    {minimum: Settings.password_length_min}
+    {minimum: Settings.password_length_min}, allow_nil: true
   has_secure_password
 
   class << self
