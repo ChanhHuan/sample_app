bab9b72d71e2de18431834dbc316e508d3189295
huan <nguyenchanhhuan@gmail.com>
2019-03-14 10:01:38 +0700

    chapter 14

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,9 @@ class Micropost < ApplicationRecord
   belongs_to :user
   scope :sort_by_created_at, ->{order created_at: :desc}
   scope :user_id, ->(id){where "user_id = ?", id}
+  scope :find_user_id, (lambda do |following_ids, id|
+    (where "user_id IN (?) OR user_id = ?", following_ids, id)
+  end)
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: Settings.micropost_max}
--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,14 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.i18n.available_locales = [:en]
     config.i18n.default_locale = :en
     config.middleware.use I18n::JS::Middleware
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
