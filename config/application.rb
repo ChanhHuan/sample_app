fbcd555af7e96c4621aa338bac1a4534aa923f0f
huan <nguyenchanhhuan@gmail.com>
2019-03-21 14:10:45 +0700

    finish

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ module SampleApp
     config.i18n.default_locale = :en
     config.middleware.use I18n::JS::Middleware
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
   end
 end
